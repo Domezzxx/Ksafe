a58f5646244da547f77ab66b4f64c0f5d9be77c2
Fanta1049 <milk4ta@gmail.com>
2026-04-18 16:08:37 +0700

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // 💡 เพิ่มการนำเข้า Firestore
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB937hfvZjrN0lMax8f2MGSOxuM7VUegrE",
+  authDomain: "ksafe-2cb9f.firebaseapp.com",
+  databaseURL: "https://ksafe-2cb9f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ksafe-2cb9f",
+  storageBucket: "ksafe-2cb9f.firebasestorage.app",
+  messagingSenderId: "257440695264",
+  appId: "1:257440695264:web:7e5e0608ecee3aedc4a185",
+  measurementId: "G-9FRHXPPW3X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// 💡 สร้างตัวแปร db และ export ออกไปใช้งาน
+export const db = getFirestore(app);
