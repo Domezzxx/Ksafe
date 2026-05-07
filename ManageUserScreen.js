f7cf0650680c804b935c72dab752957c9c4a355e
Fanta1049 <milk4ta@gmail.com>
2026-05-07 23:45:16 +0700

    Update ManageUserScreen.js

--- ManageUserScreen.js
+++ ManageUserScreen.js
@@ -264,7 +264,7 @@ const styles = StyleSheet.create({
   userCard: { 
     flexDirection: 'row', 
     alignItems: 'center', 
-    backgroundColor: '#F9FAFB', 
+    backgroundColor: '#F5F5F5', 
     marginHorizontal: 20, 
     marginBottom: 10, 
     padding: 15, 
