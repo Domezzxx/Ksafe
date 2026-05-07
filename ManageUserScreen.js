a606ebf1603c3643c3601b1ff79bdbb418822da1
Fanta1049 <milk4ta@gmail.com>
2026-05-07 22:27:46 +0700

    Update ManageUserScreen.js

--- ManageUserScreen.js
+++ ManageUserScreen.js
@@ -163,7 +163,7 @@ export default function ManageUserScreen({ onGoHome, onGoSOS, onGoSearch, onGoPr
         {/* Footer Navigation */}
                 <View style={styles.footer}>
                   <TouchableOpacity style={styles.footerButton} onPress={onGoHome}><Image source={require('./assets/home (2).png')} style={[styles.footerIcon, { tintColor: '#D9D9D9' }]} /></TouchableOpacity>
-                  <TouchableOpacity style={styles.footerButton} onPress={onGoSOS}><Image source={require('./assets/emergency (1).png')} style={[styles.footerIcon, { tintColor: '#D9D9D9' }]} /></TouchableOpacity>
+                  <TouchableOpacity style={styles.footerButton} onPress={onGoSOS}><Image source={require('./assets/phone-call.png')} style={[styles.footerIcon, { tintColor: '#D9D9D9' }]} /></TouchableOpacity>
                   <TouchableOpacity style={styles.footerButton} onPress={onGoSearch}><Image source={require('./assets/map (1).png')} style={[styles.footerIcon, { tintColor: '#D9D9D9' }]} /></TouchableOpacity>
                   <TouchableOpacity style={styles.footerButton} onPress={onGoProfile}><Image source={require('./assets/user.png')} style={[styles.footerIcon, { tintColor: '#F87C47' }]} /></TouchableOpacity>
                 </View>
@@ -284,7 +284,7 @@ const styles = StyleSheet.create({
   saveBtnText: { color: '#fff', fontWeight: 'bold', fontSize: 18 },
   deleteBtn: { backgroundColor: '#EF4444', paddingVertical: 12, borderRadius: 12, flex: 1, alignItems: 'center' },
   deleteBtnText: { color: '#fff', fontWeight: 'bold', fontSize: 18 },
-  footer: { position: 'absolute', bottom: 15, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', width: '100%', height: 80, backgroundColor: '#FFF', borderTopWidth: 1, borderTopColor: '#F0F0F0', paddingBottom: 15 },
+  footer: { position: 'absolute', bottom: 0, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', width: '100%', height: 80, backgroundColor: '#FFF', borderTopWidth: 1, borderTopColor: '#F0F0F0', paddingBottom: 15 },
   footerButton: { padding: 10, flex: 1, alignItems: 'center' },
   footerIcon: { width: 25, height: 25 }
 });
